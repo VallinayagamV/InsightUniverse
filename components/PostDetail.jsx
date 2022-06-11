467b1b55df91987d6e07ed87df26e5fafc585672
Vallinayagam V <valli3nayagam@gmail.com>
2022-06-11 20:42:34 +0530

    Title fix

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import moment from 'moment';
+import Head from "next/head";
 
 const PostDetail = ({ post }) => {
   const getContentFragment = (index, text, obj, type) => {
@@ -45,6 +45,13 @@ const PostDetail = ({ post }) => {
   return (
     <>
       <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
+        <Head>
+          <title>{post.title}</title>
+          <meta
+            name="description"
+            content={post.content}
+          />
+        </Head>
         <div className="relative overflow-hidden shadow-md mb-6">
           <img src={post.featuredImage.url} alt="" className="object-top h-full w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" />
         </div>
